21c45b40bd29a0bcffcf0e9402630677f50542bb
turko2373 <turko1073@gmail.com>
2022-12-18 23:11:08 -0300

    add routes get all users and get an user

--- config/db.py
+++ config/db.py
@@ -7,6 +7,4 @@ from sqlalchemy import create_engine, MetaData
 
 engine = create_engine("mysql+pymysql://turko:123@localhost:3306/fastapi_users")
 
-conn = engine.connect()
-
 meta_data = MetaData()
--- router/router.py
+++ router/router.py
@@ -5,7 +5,7 @@ from fastapi import APIRouter, Response
 # Internal Module
 
 from schema.user_schema import UserSchema
-from config.db import conn
+from config.db import engine
 from model.users import users
 
 # Werkzeug
@@ -15,6 +15,10 @@ from werkzeug.security import generate_password_hash, check_password_hash
 # Starlette
 
 from starlette import status
+
+# Typing
+
+from typing import List
 
 
 user = APIRouter()
@@ -32,14 +36,46 @@ def root():
 
 @user.post("/api/user", status_code = status.HTTP_201_CREATED)
 def create_user(data_user: UserSchema):
-    new_user = data_user.dict()
+    with engine.connect() as conn:
+        new_user = data_user.dict()
+        
+        # Encryption 
+        
+        new_user["user_passw"] = generate_password_hash(data_user.user_passw, "pbkdf2:sha256:30", 30)
+        
+        # Writing to database
+        
+        conn.execute(users.insert().values(new_user))    
+        return Response(status_code = status.HTTP_201_CREATED)
+
+
+## Get all users
+
+@user.get("/api/user", response_model = List[UserSchema])
+def get_users():
+    with engine.connect() as conn:
+        result = conn.execute(users.select()).fetchall()
+        return result
     
-    # Encryption 
     
-    new_user["user_passw"] = generate_password_hash(data_user.user_passw, "pbkdf2:sha256:30", 30)
+# Get User
+
+@user.get("/api/user/{user_id}", response_model = UserSchema)
+def get_user(user_id: str):
+    with engine.connect() as conn:
+        result = conn.execute(users.select().where(users.c.id == user_id)).first()
+        
+        return result
     
-    # Writing to database
     
-    conn.execute(users.insert().values(new_user))    
-    return Response(status_code = status.HTTP_201_CREATED)
-    
+# Update User
+
+@user.put("/api/user/{user_id}")
+def update_user(data_update: UserSchema, user_id: str):
+    with engine.connect() as conn:
+        encrypt_passw = generate_password_hash(data_update.user_passw, "pbkdf2:sha256:30", 30)
+        conn.execute(users.update().values(name = data_update.name, username = data_update.username, user_passw = encrypt_passw).where(users.c.id == user_id))
+        
+        result = conn.execute(users.select().where(users.c.id == user_id)).first()
+        
+        return result
